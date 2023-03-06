a828ab136bd68e006faab3dcce16f712ca388310
Abhijit Malvadkar <abhijit@sorigin.com>
2023-03-06 17:19:43 +0530

    Added Production enviornement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherAPIBaseURL:'https://weatherapi-com.p.rapidapi.com/forecast.json',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'0663609dd3msh2ad73cef71b6c94p1a967ejsn2e4be315c1bd'
 };
